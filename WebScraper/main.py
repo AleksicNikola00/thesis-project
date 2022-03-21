fb1d51403dc5b360fc2cd5d19ccf510cbf646ca9
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-03-21 18:33:31 +0100

    feat:Added product model and scraper for fashion and friends site

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
 
 import web_locations
+from model.product import Product
+
+product_list = []
 
 
 def open_file():
@@ -10,12 +12,36 @@ def open_file():
     soup = BeautifulSoup(html_text, 'lxml')
     products = soup.find_all('li', class_="item product product-item-info product-item col-lg-4 col-md-4 col-sm-4 col-xs-6")
     for product in products:
-        print(product.text)
-        print(product.find('a')['href'])
+        link = product.find('a')['href']
+        add_product(link)
+
+        
+def add_product(link):
+    product_html = requests.get(link).text
+    product_soup = BeautifulSoup(product_html, 'lxml')
+
+    product_info = product_soup.find('div', class_='product-info-main')
+    brand = product_info.a['title']
+    model = product_info.find('div', class_='product attribute overview').div.text
+    price = convert_to_num(product_info.find('span', class_='price').text)
+    product_list.append(Product(brand=brand, model=model, price=price, link=link))
 
 
-# Press the green button in the gutter to run the script.
+# form 32.232,00 RSD
+def convert_to_num(number_string):
+    num = number_string.split(' ')[0]
+    num = num.replace('.', '')
+    num = num.split(',')[0]
+    return float(num)
+
+
+def print_list():
+    for product in product_list:
+        product.to_string()
+        print('\n')
+
 if __name__ == '__main__':
     open_file()
+    print_list()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
