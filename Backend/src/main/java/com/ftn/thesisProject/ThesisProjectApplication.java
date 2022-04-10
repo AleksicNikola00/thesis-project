c536b4cbe4ecff0df28cf0ef47f3b3ea2b2373b3
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-04-10 16:56:17 +0200

    feat: added maven project

---
+++ Backend/src/main/java/com/ftn/thesisProject/ThesisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.thesisProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisProjectApplication.class, args);
+	}
+
+}
