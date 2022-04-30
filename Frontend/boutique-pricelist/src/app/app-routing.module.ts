8b4ccf21ba46f8e2b2ced9b394c6c5fbddcb78ce
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-04-30 17:47:12 +0200

    feat: shoes and clothes components initialized

--- Frontend/boutique-pricelist/src/app/app-routing.module.ts
+++ Frontend/boutique-pricelist/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ClothesComponent } from './products/clothes/clothes.component';
+import { ShoesComponent } from './products/shoes/shoes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'products/clothes', component: ClothesComponent},
+  {path: 'products/shoes', component: ShoesComponent},
+  {path: '**', component: HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/boutique-pricelist/src/app/app.module.ts
+++ Frontend/boutique-pricelist/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ShoesComponent } from './products/shoes/shoes.component';
+import { ClothesComponent } from './products/clothes/clothes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ShoesComponent,
+    ClothesComponent
   ],
   imports: [
     BrowserModule,
