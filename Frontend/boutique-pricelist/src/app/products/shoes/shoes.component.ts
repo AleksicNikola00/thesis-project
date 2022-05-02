eff9c283dbf1f6e9cfc5d0bb1a425cef0ae38597
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2022-05-03 00:17:30 +0200

    feat: added shoes view to frontend

--- Frontend/boutique-pricelist/src/app/products/shoes/shoes.component.ts
+++ Frontend/boutique-pricelist/src/app/products/shoes/shoes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/model/IProduct';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-shoes',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoesComponent implements OnInit {
 
-  constructor() { }
+  shoes: IProduct[] = [];
+
+  constructor(private _productService : ProductService) { }
 
   ngOnInit(): void {
+   this._productService.getShoes().subscribe(
+     value => this.shoes = value,
+     error => console.log(error)
+   );
   }
 
 }
