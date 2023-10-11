d6a704162c6ec227958e535973ef4f8ef9b66c3b
Nikola Aleksić <73480578+AleksicNikola00@users.noreply.github.com>
2023-10-11 18:07:31 +0200

    Refactored homepage

--- Frontend/boutique-pricelist-react/src/shared/ui/pages/HomePage.jsx
+++ Frontend/boutique-pricelist-react/src/shared/ui/pages/HomePage.jsx
@@ -12,21 +12,21 @@ const HomePage = () => {
   };
 
   return (
-    <div className="flex h-full px-10 justify-center items-center gap-10  dark:bg-neutral-950 dark:text-white">
+    <div className="flex h-full px-10 py-7 justify-center items-center gap-10  dark:bg-neutral-950 dark:text-white">
       <button
         onClick={onClothesHandler}
-        className="w-1/2 h-5/6 bg-zinc-300 dark:bg-neutral-900"
+        className="w-1/2 h-full bg-zinc-300 dark:bg-neutral-900"
       >
         <img
           src="/pngs/clothes.png"
           className="w-full h-full object-cover
-        transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+        transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105"
         />
       </button>
       <button
         onClick={onShoesHandler}
-        className="w-1/2 h-5/6 bg-zinc-300 dark:bg-neutral-900
-        transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"
+        className="w-1/2 h-full bg-zinc-300 dark:bg-neutral-900
+        transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105"
       >
         <img
           src="/pngs/air-force-1.png"
